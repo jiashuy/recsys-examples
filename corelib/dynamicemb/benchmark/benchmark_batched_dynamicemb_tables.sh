a5fd98e7611b8b50dbe7066edd202718ca0e01d4
Jiashu Yao <jiashu.yao.cn@gmail.com>
2025-10-20 10:37:31 +0000

    test benchmakr

--- corelib/dynamicemb/benchmark/benchmark_batched_dynamicemb_tables.sh
+++ corelib/dynamicemb/benchmark/benchmark_batched_dynamicemb_tables.sh
@@ -2,8 +2,8 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
-optimizer_types=("adam" "sgd")
-batch_sizes=(1048576 65536)
+optimizer_types=("adam")
+batch_sizes=(1048576)
 embedding_dims=(128)
 alphas=(1.05)
 use_index_dedups=("False")
@@ -13,50 +13,50 @@ gpu_ratio=0.1
 declare -A hbm=(["sgd"]=12.8 ["adam"]=38.4)
 
 rm benchmark_results.json
-for batch_size in "${batch_sizes[@]}"; do
-  echo "batch_size: $batch_size"
-  for capacity in "${capacities[@]}"; do
-    echo "capacity: $capacity"
-    for optimizer_type in "${optimizer_types[@]}"; do
-      echo "optimizer_type: $optimizer_type"
-      for embedding_dim in "${embedding_dims[@]}"; do
-        echo "embedding_dim: $embedding_dim"
-        for alpha in "${alphas[@]}"; do
-          echo "alpha: $alpha"
+# for batch_size in "${batch_sizes[@]}"; do
+#   echo "batch_size: $batch_size"
+#   for capacity in "${capacities[@]}"; do
+#     echo "capacity: $capacity"
+#     for optimizer_type in "${optimizer_types[@]}"; do
+#       echo "optimizer_type: $optimizer_type"
+#       for embedding_dim in "${embedding_dims[@]}"; do
+#         echo "embedding_dim: $embedding_dim"
+#         for alpha in "${alphas[@]}"; do
+#           echo "alpha: $alpha"
 
-          torchrun --nnodes 1 --nproc_per_node 1 \
-              ./benchmark/benchmark_batched_dynamicemb_tables.py  \
-                --caching \
-                --cache_algorithm "lru" \
-                --gpu_ratio $gpu_ratio \
-                --batch_size $batch_size \
-                --num_embeddings_per_feature $capacity \
-                --embedding_dim $embedding_dim \
-                --hbm_for_embeddings ${hbm[$optimizer_type]} \
-                --optimizer_type $optimizer_type \
-                --feature_distribution "pow-law" \
-                --alpha $alpha \
-                --num_iterations 100 \
+#           torchrun --nnodes 1 --nproc_per_node 1 \
+#               ./benchmark/benchmark_batched_dynamicemb_tables.py  \
+#                 --caching \
+#                 --cache_algorithm "lru" \
+#                 --gpu_ratio $gpu_ratio \
+#                 --batch_size $batch_size \
+#                 --num_embeddings_per_feature $capacity \
+#                 --embedding_dim $embedding_dim \
+#                 --hbm_for_embeddings ${hbm[$optimizer_type]} \
+#                 --optimizer_type $optimizer_type \
+#                 --feature_distribution "pow-law" \
+#                 --alpha $alpha \
+#                 --num_iterations 100 \
 
-          torchrun --nnodes 1 --nproc_per_node 1 \
-              ./benchmark/benchmark_batched_dynamicemb_tables.py  \
-                --batch_size $batch_size \
-                --num_embeddings_per_feature $capacity \
-                --embedding_dim $embedding_dim \
-                --hbm_for_embeddings ${hbm[$optimizer_type]} \
-                --optimizer_type $optimizer_type \
-                --feature_distribution "pow-law" \
-                --alpha $alpha \
-                --num_iterations 100 \
-                --cache_algorithm "lru" \
+#           torchrun --nnodes 1 --nproc_per_node 1 \
+#               ./benchmark/benchmark_batched_dynamicemb_tables.py  \
+#                 --batch_size $batch_size \
+#                 --num_embeddings_per_feature $capacity \
+#                 --embedding_dim $embedding_dim \
+#                 --hbm_for_embeddings ${hbm[$optimizer_type]} \
+#                 --optimizer_type $optimizer_type \
+#                 --feature_distribution "pow-law" \
+#                 --alpha $alpha \
+#                 --num_iterations 100 \
+#                 --cache_algorithm "lru" \
 
-          # ncu -f --target-processes all --export de_and_tr-$batch_size-$capacity-$optimizer_type-rep.report --section SchedulerStats --section WarpStateStats --import-source=yes --page raw --set full --profile-from-start no -k regex:"load_or_initialize_" \
-          # nsys profile  -s none -t cuda,nvtx,osrt,mpi,ucx -f true -o de_and_tr-$batch_size-$capacity-$optimizer_type.qdrep -c cudaProfilerApi  --cpuctxsw none --cuda-flush-interval 100 --capture-range-end=stop --cuda-graph-trace=node \
-        done
-      done
-    done
-  done
-done
+#           # ncu -f --target-processes all --export de_and_tr-$batch_size-$capacity-$optimizer_type-rep.report --section SchedulerStats --section WarpStateStats --import-source=yes --page raw --set full --profile-from-start no -k regex:"load_or_initialize_" \
+#           # nsys profile  -s none -t cuda,nvtx,osrt,mpi,ucx -f true -o de_and_tr-$batch_size-$capacity-$optimizer_type.qdrep -c cudaProfilerApi  --cpuctxsw none --cuda-flush-interval 100 --capture-range-end=stop --cuda-graph-trace=node \
+#         done
+#       done
+#     done
+#   done
+# done
 
 capacities=("24")
 gpu_ratio=1.0
@@ -73,6 +73,7 @@ for batch_size in "${batch_sizes[@]}"; do
         for alpha in "${alphas[@]}"; do
           echo "alpha: $alpha"
 
+          nsys profile  -s none -t cuda,nvtx,osrt,mpi,ucx -f true -o de_and_tr-10.20.qdrep -c cudaProfilerApi  --cpuctxsw none --cuda-flush-interval 100 --capture-range-end=stop --cuda-graph-trace=node \
           torchrun --nnodes 1 --nproc_per_node 1 \
               ./benchmark/benchmark_batched_dynamicemb_tables.py  \
                 --batch_size $batch_size \
